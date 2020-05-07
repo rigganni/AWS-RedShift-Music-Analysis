42c199a885795e27b987fe353ff5629f1e17a5c1
Nick Riggan <riggan@pm.me>
2020-05-07 08:23:34 -0400

    refactor: Change RedShift connection string variables
    Change RedShift connection string variables in create_tables.py to
    different config section.

--- create_tables.py
+++ create_tables.py
@@ -19,7 +19,13 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    DWH_HOST = config.get("DWH", "DWH_HOST")
+    DWH_DB= config.get("DWH","DWH_DB")
+    DWH_DB_USER= config.get("DWH","DWH_DB_USER")
+    DWH_DB_PASSWORD= config.get("DWH","DWH_DB_PASSWORD")
+    DWH_PORT = config.get("DWH","DWH_PORT")
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(DWH_HOST, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, DWH_PORT))
     cur = conn.cursor()
 
     drop_tables(cur, conn)
